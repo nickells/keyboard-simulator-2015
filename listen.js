6905c87fcebc6a4c252e99db07e70d9eb3650d12
nickells <ellsworth.nick@gmail.com>
2015-11-21 18:04:31 -0500

    debounced listener

--- listen.js
+++ listen.js
@@ -1,9 +1,13 @@
-
-var keyDown = function(){
-  var keyDownSound = new Audio('keydown.wav');
-  keyDownSound.play();
+var lastKey;
+var keyDown = function(e){
+  if (e.which !== lastKey){
+    var keyDownSound = new Audio('keydown.wav');
+    keyDownSound.play();   
+  }
+  lastKey = e.which;
 };
-var keyUp = function(){
+var keyUp = function(e){
+  lastKey = undefined;
   var keyUpSound = new Audio('keyup.wav');
   keyUpSound.play();
 };
